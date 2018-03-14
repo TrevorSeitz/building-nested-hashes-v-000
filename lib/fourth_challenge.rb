f692d36aa77bb3f4b1c03d3d5b43d4abc94768b6
Trevor Seitz <trevinla@gmail.com>
2018-03-14 20:13:45 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -22,5 +22,5 @@ def fourth_challenge
 
 end
 
-epic_tragedy[:montague][:hero][status:] = "dead"
-epic_tragedy[:capulet][:heroine] [status:] = "dead"
+epic_tragedy[:montague][:hero][status:]="dead"
+epic_tragedy[:capulet][:heroine] [status:]="dead"
